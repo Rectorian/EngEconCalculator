e9d6e6ab7cb1a0bf0c6c6aa00e7ff994c2dd65fa
Rectorian <rykuskie@gmail.com>
2024-09-04 09:18:42 -0500

    Added module to contain the non-testing pieces of code. Implemented a display text with color for a non-RGB color value

--- src/lib.rs
+++ src/lib.rs
@@ -7,41 +7,53 @@ pub fn test_ansi() {
     let octal_esc = 0o033;
     let ansi_code_esc: char = '\x1B';
 
+    //Sequence that will cleear the screen and reset the cursor to the front position.
     let ansi_seq_clear_screen: &str = "\x1B[2J\x1B[H";
 
     println!("{}Ringing a Bell (hopefully)", ansi_seq_clear_screen);
     println!("{}This is some test text", ansi_code_bell);
     //println!("{}{}This is some test text", ansi_code_esc, ansi_code_bell);
 
+    //Sequence to set background color to red.
     let ansi_color_sequence: &str = "\x1B[48:2:255:0:0m";
-    //The following will print the text red in both iterm2 and the default macOS terminal
-    //My suspicion was that for some reason, only text contained within the string literal will be
-    //printed with the correct coloring, and I was right. Will need to figure out how to get around
-    //this. The library I looked at seemed to combine everything before sending to the output?
-    let ansi_color_picked: &str = "\x1B[31mThis text will print red";
-    let ansi_text_color_sequence: &str = "\x1B[38:2:0:128:128m"; //Don't use spaces within the
-                                                                 //sequence like the following [38 2;0;255;128m"; it causes an issue for some reason
-                                                                 //A test to see if the 8 bit colors work correctly in the macOS terminal
+
+    //This sequence will print the the text red until reset or changed.
+    //NOTE: While iTerm2 supports 8-bit color, the default macOS terminal does not. Use the other
+    //color representation (5;ColorNumberHere)
+    let ansi_color_picked: &str = "\x1B[38;2;255;0;0mThis text will print red";
+    let ansi_text_color_sequence: &str = "\x1B[38:2:0:128:128m";
+    //Don't use spaces within the sequence like the following [38 2;0;255;128m"; it causes an issue for some reason
+
+    //A test to see if the 8 bit colors work correctly in the macOS terminal
     let mut combined_fore_and_back = String::new();
     format!("{}{}", ansi_color_picked, ansi_text_color_sequence);
 
     let ansi_non_256_background_color: &str = "\x1B[48;5;11m";
 
-    //From the way this works, it seems that after we set the background to red, it seems that this
-    //becomes thedefault?
-    //Text printed after this using the println! macro can be adjusted to be different colors and
-    //backgrounds, but for some reason, all the white space afterwards has a red background too,
-    //but just the first line
-    //Must be due to the underlying way that rust implements concatenation using macros? I don't
-    //have enough experience to make a specific determination
+    //A statement that will result in text with a red background
+    //Due to not resetting the color formatting before the newline is created, all the space up
+    //until the first character of the next print statement will also have a red background
     //println!("{}{}This text's background should be red", ansi_color_sequence, ansi_text_color_sequence);
 
-    //So I think the way that rust concatenate strings, it does it in a way that seems to get rid
-    //of some of the escaped stuff?
+    //Me messing around with different ways to make the string to try and solve the issue that
+    //ended up being me not understanding that the default macOS terminal does not have 8-bit color
+    //for ANSI
     combined_fore_and_back += "This is some text I want to print";
 
     println!("{}", combined_fore_and_back);
 
     println!("\x1B[0mThe following text should be red");
     println!("{}", ansi_color_picked);
+}
+
+pub mod ansi_commands {
+    pub const ANSI_CLEAR: &str = "\x1B[2J\x1B[H";
+    pub const ANSI_RESET_COL: &str = "\x1B[38m";
+
+    //An interesting facet, it seems that anything printed before I call a color change again, it
+    //seems to print with the formatting previously given from the ansi_color_picked &str slice
+    pub fn disp_text_colored(text: String, color_val: u8) -> String {
+        let set_color_sequence: String = format!("\x1B[38;5;{}m", color_val);
+        format!("{}{}{}", set_color_sequence, text, ANSI_RESET_COL)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     EngEconCalculator::test_ansi();
+    use EngEconCalculator::ansi_commands;
+
+    //print!("{}", ansi_commands::ANSI_CLEAR);
+    println!(
+        "{}",
+        ansi_commands::disp_text_colored(String::from("This is a test"), 13)
+    );
 }
