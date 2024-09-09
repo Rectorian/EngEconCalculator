197777b4bc0e57b008cef97ff5a14ca8f0a08d55
Rectorian <rykuskie@gmail.com>
2024-09-08 19:01:09 -0500

    Began implementing simple UI for selecting which calculation to perform. Also will continuously loop until an exit option is selected

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod invest_items;
 pub mod ansi_commands {
     const ANSI_CLEAR: &str = "\x1B[3J\x1B[2J\x1B[H";
     const ANSI_RESET_COL: &str = "\x1B[39m";
+    const RS_ON: bool = false;
 
     //Function to get colored text and reset it.
     pub fn get_text_colored(text: &str, color_val: u8) -> String {
@@ -10,8 +11,12 @@ pub mod ansi_commands {
         format!("{}{}{}", set_color_sequence, text, ANSI_RESET_COL)
     }
 
-    pub fn clear_screen() {
-        print!("{ANSI_CLEAR}");
+    pub fn clear_screen(respect_reset: bool) {
+        if !respect_reset {
+            print!("{ANSI_CLEAR}")
+        } else if RS_ON {
+            print!("{ANSI_CLEAR}");
+        }
     }
 
     pub fn disable_cursor() {
@@ -24,7 +29,7 @@ pub mod ansi_commands {
 }
 
 pub mod user_interface {
-    fn grab_user_input(text_prompt: &str) -> String {
+    pub fn grab_user_input(text_prompt: &str) -> String {
         let mut output = String::new();
 
         println!("{}", text_prompt);
@@ -36,7 +41,7 @@ pub mod user_interface {
         output
     }
 
-    fn grab_user_num(text_prompt: &str) -> f64 {
+    pub fn grab_user_num(text_prompt: &str) -> f64 {
         let mut input = grab_user_input(text_prompt);
         let out_num: f64;
 
@@ -56,7 +61,7 @@ pub mod user_interface {
         out_num
     }
 
-    fn grab_user_num_restricted(text_prompt: &str, low: f64, high: f64) -> f64 {
+    pub fn grab_user_num_restricted(text_prompt: &str, low: f64, high: f64) -> f64 {
         let mut out_num: f64;
 
         while {
@@ -76,7 +81,7 @@ pub mod user_interface {
             invest_items::{data, investment_calculations},
         };
 
-        cs();
+        cs(true);
 
         let principal = data::AmountType::Principal(data::Amount::Fl64(grab_user_num(
             "Enter Principal Amount:",
@@ -89,18 +94,326 @@ pub mod user_interface {
         let periods = data::AmountType::TimePeriods(data::Amount::In32(grab_user_num(
             "Enter Number of Compounding Periods:",
         ) as i32));
-        cs();
+        cs(true);
 
         let result = match investment_calculations::calculations::f_from_p(
             &principal, &periods, &interest,
         ) {
             Ok(num) => {
-                let out_num_format = format!("{:.2}", num);
+                let out_num_format = format!("${:.2}", num);
                 gtc(out_num_format.as_str(), 10)
             }
             Err(err_mes) => gtc(err_mes.as_str(), 9),
         };
 
         println!("Final amount calculated as: {}", result);
+    }
+
+    pub fn user_p_from_f() {
+        use super::{
+            ansi_commands::{clear_screen as cs, get_text_colored as gtc},
+            invest_items::{data, investment_calculations},
+        };
+
+        cs(true);
+
+        let final_ =
+            data::AmountType::Final(data::Amount::Fl64(grab_user_num("Enter Final Amount:")));
+
+        let interest = data::AmountType::InterestRate(data::InterestType::Compound(grab_user_num(
+            "Enter Interest Rate as Decimal:",
+        )));
+
+        let periods = data::AmountType::TimePeriods(data::Amount::In32(grab_user_num(
+            "Enter Number of Compounding Periods:",
+        ) as i32));
+
+        cs(true);
+
+        let result =
+            match investment_calculations::calculations::p_from_f(&final_, &periods, &interest) {
+                Ok(num) => {
+                    let out_num_format = format!("${:.2}", num);
+                    gtc(out_num_format.as_str(), 10)
+                }
+                Err(err_mes) => gtc(err_mes.as_str(), 9),
+            };
+
+        println!("Principal amount calculated as: {}", result);
+    }
+
+    pub fn user_p_from_a() {
+        use super::{
+            ansi_commands::{clear_screen as cs, get_text_colored as gtc},
+            invest_items::{data, investment_calculations},
+        };
+
+        cs(true);
+
+        let uniform =
+            data::AmountType::Uniform(data::Amount::Fl64(grab_user_num("Enter Uniform Amount:")));
+
+        let interest = data::AmountType::InterestRate(data::InterestType::Compound(grab_user_num(
+            "Enter Interest Rate as Decimal:",
+        )));
+
+        let periods = data::AmountType::TimePeriods(data::Amount::In32(grab_user_num(
+            "Enter Number of Compounding Periods:",
+        ) as i32));
+
+        cs(true);
+
+        let result =
+            match investment_calculations::calculations::p_from_a(&uniform, &periods, &interest) {
+                Ok(num) => {
+                    let out_num_format = format!("${:.2}", num);
+                    gtc(out_num_format.as_str(), 10)
+                }
+                Err(err_mes) => gtc(err_mes.as_str(), 9),
+            };
+
+        println!("Minimum Principal for {}: {}", uniform, result);
+    }
+
+    pub fn user_a_from_p() {
+        use super::{
+            ansi_commands::{clear_screen as cs, get_text_colored as gtc},
+            invest_items::{data, investment_calculations},
+        };
+
+        cs(true);
+
+        let principal = data::AmountType::Principal(data::Amount::Fl64(grab_user_num(
+            "Enter Principal Amount:",
+        )));
+
+        let interest = data::AmountType::InterestRate(data::InterestType::Compound(grab_user_num(
+            "Enter Interest Rate as Decimal:",
+        )));
+
+        let periods = data::AmountType::TimePeriods(data::Amount::In32(grab_user_num(
+            "Enter Number of Compounding Periods:",
+        ) as i32));
+
+        cs(true);
+
+        let result = match investment_calculations::calculations::a_from_p(
+            &principal, &periods, &interest,
+        ) {
+            Ok(num) => {
+                let out_num_format = format!("${:.2}", num);
+                gtc(out_num_format.as_str(), 10)
+            }
+            Err(err_mes) => gtc(err_mes.as_str(), 9),
+        };
+
+        println!("Maximum Withdrawal Amount for {}: {}", principal, result);
+    }
+
+    pub fn user_f_from_a() {
+        use super::{
+            ansi_commands::{clear_screen as cs, get_text_colored as gtc},
+            invest_items::{data, investment_calculations},
+        };
+
+        cs(true);
+
+        let uniform =
+            data::AmountType::Uniform(data::Amount::Fl64(grab_user_num("Enter Uniform Amount:")));
+
+        let interest = data::AmountType::InterestRate(data::InterestType::Compound(grab_user_num(
+            "Enter Interest Rate as Decimal:",
+        )));
+
+        let periods = data::AmountType::TimePeriods(data::Amount::In32(grab_user_num(
+            "Enter Number of Compounding Periods:",
+        ) as i32));
+
+        cs(true);
+
+        let result =
+            match investment_calculations::calculations::f_from_a(&uniform, &periods, &interest) {
+                Ok(num) => {
+                    let out_num_format = format!("${:.2}", num);
+                    gtc(out_num_format.as_str(), 10)
+                }
+                Err(err_mes) => gtc(err_mes.as_str(), 9),
+            };
+
+        println!("Maximum Final Amount for {}: {}", uniform, result);
+    }
+
+    pub fn user_a_from_f() {
+        use super::{
+            ansi_commands::{clear_screen as cs, get_text_colored as gtc},
+            invest_items::{data, investment_calculations},
+        };
+
+        cs(true);
+
+        let final_ =
+            data::AmountType::Final(data::Amount::Fl64(grab_user_num("Enter Final Amount:")));
+
+        let interest = data::AmountType::InterestRate(data::InterestType::Compound(grab_user_num(
+            "Enter Interest Rate as Decimal:",
+        )));
+
+        let periods = data::AmountType::TimePeriods(data::Amount::In32(grab_user_num(
+            "Enter Number of Compounding Periods:",
+        ) as i32));
+
+        cs(true);
+
+        let result =
+            match investment_calculations::calculations::a_from_f(&final_, &periods, &interest) {
+                Ok(num) => {
+                    let out_num_format = format!("${:.2}", num);
+                    gtc(out_num_format.as_str(), 10)
+                }
+                Err(err_mes) => gtc(err_mes.as_str(), 9),
+            };
+
+        println!("Minimum Payments Needed for {}: {}", final_, result);
+    }
+
+    pub fn user_p_from_g() {
+        use super::{
+            ansi_commands::{clear_screen as cs, get_text_colored as gtc},
+            invest_items::{data, investment_calculations},
+        };
+
+        cs(true);
+
+        let gradient =
+            data::AmountType::Gradient(data::Amount::Fl64(grab_user_num("Enter Gradient Amount:")));
+
+        let interest = data::AmountType::InterestRate(data::InterestType::Compound(grab_user_num(
+            "Enter Interest Rate as Decimal:",
+        )));
+
+        let periods = data::AmountType::TimePeriods(data::Amount::In32(grab_user_num(
+            "Enter Number of Compounding Periods:",
+        ) as i32));
+
+        cs(true);
+
+        let result =
+            match investment_calculations::calculations::p_from_g(&gradient, &periods, &interest) {
+                Ok(num) => {
+                    let out_num_format = format!("${:.2}", num);
+                    gtc(out_num_format.as_str(), 10)
+                }
+                Err(err_mes) => gtc(err_mes.as_str(), 9),
+            };
+
+        println!("Present Worth of {}: {}", gradient, result);
+    }
+
+    pub fn user_a_from_g() {
+        use super::{
+            ansi_commands::{clear_screen as cs, get_text_colored as gtc},
+            invest_items::{data, investment_calculations},
+        };
+
+        cs(true);
+
+        let gradient =
+            data::AmountType::Gradient(data::Amount::Fl64(grab_user_num("Enter Gradient Amount:")));
+
+        let interest = data::AmountType::InterestRate(data::InterestType::Compound(grab_user_num(
+            "Enter Interest Rate as Decimal:",
+        )));
+
+        let periods = data::AmountType::TimePeriods(data::Amount::In32(grab_user_num(
+            "Enter Number of Compounding Periods:",
+        ) as i32));
+
+        cs(true);
+
+        let result =
+            match investment_calculations::calculations::a_from_g(&gradient, &periods, &interest) {
+                Ok(num) => {
+                    let out_num_format = format!("${:.2}", num);
+                    gtc(out_num_format.as_str(), 10)
+                }
+                Err(err_mes) => gtc(err_mes.as_str(), 9),
+            };
+
+        println!("Equivalent Uniform Payment for {}: {}", gradient, result);
+    }
+
+    pub fn user_f_from_g() {
+        use super::{
+            ansi_commands::{clear_screen as cs, get_text_colored as gtc},
+            invest_items::{data, investment_calculations},
+        };
+
+        cs(true);
+
+        let gradient =
+            data::AmountType::Gradient(data::Amount::Fl64(grab_user_num("Enter Gradient Amount:")));
+
+        let interest = data::AmountType::InterestRate(data::InterestType::Compound(grab_user_num(
+            "Enter Interest Rate as Decimal:",
+        )));
+
+        let periods = data::AmountType::TimePeriods(data::Amount::In32(grab_user_num(
+            "Enter Number of Compounding Periods:",
+        ) as i32));
+
+        cs(true);
+
+        let result =
+            match investment_calculations::calculations::f_from_g(&gradient, &periods, &interest) {
+                Ok(num) => {
+                    let out_num_format = format!("${:.2}", num);
+                    gtc(out_num_format.as_str(), 10)
+                }
+                Err(err_mes) => gtc(err_mes.as_str(), 9),
+            };
+
+        println!("Future Worth of {}: {}", gradient, result);
+    }
+
+    pub fn user_p_from_g_rate() {
+        use super::{
+            ansi_commands::{clear_screen as cs, get_text_colored as gtc},
+            invest_items::{data, investment_calculations},
+        };
+
+        cs(true);
+
+        let gradient = data::AmountType::GradientRate(data::InterestType::Compound(grab_user_num(
+            "Enter Gradient Rate as Decimal:",
+        )));
+
+        let initial_payment = data::AmountType::Uniform(data::Amount::Fl64(grab_user_num(
+            "Enter the Initial Payment Amount:",
+        )));
+
+        let interest = data::AmountType::InterestRate(data::InterestType::Compound(grab_user_num(
+            "Enter Interest Rate as Decimal:",
+        )));
+
+        let periods = data::AmountType::TimePeriods(data::Amount::In32(grab_user_num(
+            "Enter Number of Compounding Periods:",
+        ) as i32));
+
+        cs(true);
+
+        let result = match investment_calculations::calculations::p_from_g_rate(
+            &initial_payment,
+            &gradient,
+            &periods,
+            &interest,
+        ) {
+            Ok(num) => {
+                let out_num_format = format!("${:.2}", num);
+                gtc(out_num_format.as_str(), 10)
+            }
+            Err(err_mes) => gtc(err_mes.as_str(), 9),
+        };
+
+        println!("Future Worth of {}: {}", gradient, result);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,66 @@
 fn main() {
-    EngEconCalculator::user_interface::user_f_from_p();
+    EngEconCalculator::ansi_commands::clear_screen(false);
+    use EngEconCalculator::user_interface::*;
+
+    //user_f_from_p();
+    //user_p_from_f();
+    //user_a_from_p();
+    user_p_from_a();
+    //user_a_from_f();
+    //user_f_from_a();
 }
 
-fn test_1() {
-    EngEconCalculator::ansi_commands::clear_screen();
+fn run() {
+    EngEconCalculator::ansi_commands::clear_screen(false);
+    use EngEconCalculator::user_interface as UI;
+
+    let mut user_input = String::new();
+
+    while {
+        //Simple do part of the loop to grab the user input. Then checks for exit and returns
+        //corresponding conditional
+        user_input = UI::grab_user_input("What would you like to calculate? (Type ? for options)");
+        match user_input.to_lowercase().as_str() {
+            "e" | "end" | "exit" => false,
+            _ => true,
+        }
+    } {
+        match user_input.to_lowercase().as_str() {
+            "?" => {
+                println!("Options are:");
+                println!("\tExit (E, End, Exit)");
+                println!("\tPresent Value (P, Present)");
+                println!("\tFuture Value (F, Future)");
+                println!("\tUniform Value (A, U, Uniform)");
+                println!("\tArithmetic Gradient (AG, Arithmetic)");
+                println!("\tPresent Value Equivalent of Geometric Gradient (PG, Geometric)");
+            }
+            "p" | "present" => {
+                //TODO Put Function Here
+            }
+            "f" | "future" => {
+                //TODO Put Function Here
+            }
+            "a" | "u" | "uniform" => {
+                //TODO Put function here
+            }
+            "ag" | "arithmetic" => {
+                //TODO Put Function Here
+            }
+            "pg" | "geometric" => {
+                //TODO Put Function Here
+            }
+            _ => {
+                EngEconCalculator::ansi_commands::clear_screen(true);
+                println!("Invalid Input. Please Try Again");
+                continue;
+            }
+        }
+    }
+}
+
+fn _test_1() {
+    EngEconCalculator::ansi_commands::clear_screen(false);
     use EngEconCalculator::ansi_commands::get_text_colored as gtc;
 
     println!(
